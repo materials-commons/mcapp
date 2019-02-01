417cf71b0ee56df1be56c892ed8144c043980ff3
gtarcea <glenn.tarcea@gmail.com>
2019-01-31 21:36:13 -0500

    Async support and set max width on column

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -11,9 +11,11 @@
             </thead>
             <tbody>
             <tr v-for="project in projects">
-                <td>{{project.name}}</td>
-                <td>{{project.location ? project.location : "Not Set"}}</td>
-                <td><button class="btn btn-primary" @click="setProjectLocation(project)">Set Location</button></td>
+                <td style="max-width: 150px">{{project.name}}</td>
+                <td>{{project.location ? project.location : 'Not Set'}}</td>
+                <td>
+                    <a @click.prevent="setProjectLocation(project)" href="">Set Location</a>
+                </td>
             </tr>
             </tbody>
         </table>
@@ -36,15 +38,20 @@
                 getDirFromUser().then(dir => project.location = dir);
             }
         },
-        mounted() {
-            rest.post('/v4/ui/getProjectsForUser').then(
-                (projects) => {
-                    projects.data.forEach(p => p.location = null);
-                    this.projects = projects.data;
-                },
-                err => console.log(err)
-            )
+        async mounted() {
+            let {data} = await rest.post('/v4/ui/getProjectsForUser');
+            data.forEach(p => p.location = null);
+            this.projects = data;
         }
+        // mounted() {
+        //     rest.post('/v4/ui/getProjectsForUser').then(
+        //         (projects) => {
+        //             projects.data.forEach(p => p.location = null);
+        //             this.projects = projects.data;
+        //         },
+        //         err => console.log(err)
+        //     )
+        // }
     }
 </script>
 
